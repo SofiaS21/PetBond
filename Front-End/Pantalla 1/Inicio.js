98d470f92ba3cb30e74c8bfc108aaf7d3e74c2ca
SofiaS21 <49876490@est.ort.edu.ar>
2025-10-01 11:57:04 -0300

    AGREGANDO MAS COSAS

--- Front-End/Pantalla 1/Inicio.js
+++ Front-End/Pantalla 1/Inicio.js
@@ -1,4 +1,18 @@
-BotonPerros.addEventListener("click", FuncionBotonPerros);
-function FuncionBotonPerros (){
-    window.location.href = '../Pantalla 2/Perros.html'
+
+let BotonInicio = document.getElementById("Inicio");
+
+    function IniciarSesion() {
+        window.location.href = '../Sign In/iniciar sesion.html';
+    }
+
+    BotonInicio.addEventListener("click", IniciarSesion);
+
+
+    
+let BotonCrear = document.getElementById("Crear");
+
+function CrearCuenta() {
+    window.location.href = '../Sign In/Crear cuenta.html';
 }
+
+BotonCrear.addEventListener("click", CrearCuenta);
