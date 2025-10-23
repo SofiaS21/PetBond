beed19bb433090236c16dcc84eb4e0290c232548
SofiaS21 <49876490@est.ort.edu.ar>
2025-10-23 14:27:10 -0300

    Cambios en iniciar sesion

---
+++ Front-End/Sign in - up/Sign In/Iniciar sesion.js
@@ -0,0 +1,8 @@
+let BotonCrear = document.getElementById("Crear");
+
+
+function CrearCuenta() {
+    window.location.href = '../Sign up/Crear cuenta.html';
+}
+
+BotonCrear.addEventListener("click", CrearCuenta);
