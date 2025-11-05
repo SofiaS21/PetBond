e01d1613bfd2551c618224a7f4675218cdad3ae3
SofiaS21 <49876490@est.ort.edu.ar>
2025-11-05 10:40:07 -0300

    Perros agrenadno

---
+++ Front-End/ANIMALES/PERROS/Perros publicados/Perro 4/Perro4.js
@@ -0,0 +1,39 @@
+const boton = document.querySelector('.boton-modo');
+
+boton.addEventListener('click', () => {
+  boton.classList.toggle('activo');
+  document.body.classList.toggle('dark-mode');
+});
+
+let BotonLogo = document.getElementById("Logo");
+
+
+function Volver() {
+    window.location.href = '../../../../Pantalla 1/Inicio.html';
+}
+
+BotonLogo.addEventListener("click", Volver);
+
+
+
+let BotonPerfil = document.getElementById("Perfil");
+
+
+function CrearPerfil() {
+window.location.href = '../../../../Perfil/Perfil.html';
+}
+
+BotonPerfil.addEventListener("click", CrearPerfil);
+
+
+let BotonPublicar = document.getElementById("Publicar");
+
+
+function PublicarAnimal() {
+window.location.href = '../../../ANIMAL no publicado/Animal-anonimo.html';
+}
+
+BotonPublicar.addEventListener("click", PublicarAnimal);  
+
+
+
