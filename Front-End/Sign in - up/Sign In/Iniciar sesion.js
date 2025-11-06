2d6d7cee5fc20b5613e38b39891b589a47e841da
SofiaS21 <49876490@est.ort.edu.ar>
2025-11-06 11:22:42 -0300

    modo oscuro sign in

--- Front-End/Sign in - up/Sign In/Iniciar sesion.js
+++ Front-End/Sign in - up/Sign In/Iniciar sesion.js
@@ -1,9 +1,32 @@
 
 const boton = document.querySelector('.boton-modo');
+const imagenTelefono = document.getElementById('telefono');
+const imagenCandado = document.getElementById('Candado');
+const imagenLogo = document.getElementById('Logo');
+
 
 boton.addEventListener('click', () => {
   boton.classList.toggle('activo');
   document.body.classList.toggle('dark-mode');
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenTelefono.src = "../../imagenes/Telefono modo oscuro.png";
+  } else {
+    imagenTelefono.src = "../../imagenes/Telefono emoji.png";
+  }
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenCandado.src = "../../imagenes/Candado modo oscuro.png";
+  } else {
+    imagenCandado.src = "../../imagenes/Candado emoji.png";
+  }
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenLogo.src = "../../imagenes/Logo modo oscuro.png";
+  } else {
+    imagenLogo.src = "../../imagenes/logo-azul.png";
+  }
+
 });
 
 let BotonCrear = document.getElementById("Crear");
--- Front-End/Sign in - up/Sign up/Crear cuenta.js
+++ Front-End/Sign in - up/Sign up/Crear cuenta.js
@@ -1,6 +1,52 @@
 
+connect2Server();
 
-let BotonVolver = document.getElementById("Volver");
+const Correo = document.getElementById("Correo")
+
+FormData.addEventListener("Crear" , (e) => {
+    e.preventDefault()
+
+const input = {
+    Correo: Correo.value,
+  };
+  postEvent("registrarUsuario", input, (respuestaBack) => {
+    console.log("Respuesta del backend:", input);
+    respuesta.innerText = "Backend respondió: " + JSON.stringify(input);
+  })
+})
+
+const boton = document.querySelector('.boton-modo');
+const imagenTelefono = document.getElementById('telefono');
+const imagenCandado = document.getElementById('Candado');
+const imagenLogo = document.getElementById('Logo');
+
+
+boton.addEventListener('click', () => {
+  boton.classList.toggle('activo');
+  document.body.classList.toggle('dark-mode');
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenTelefono.src = "../../imagenes/Telefono modo oscuro.png";
+  } else {
+    imagenTelefono.src = "../../imagenes/Telefono emoji.png";
+  }
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenCandado.src = "../../imagenes/Candado modo oscuro.png";
+  } else {
+    imagenCandado.src = "../../imagenes/Candado emoji.png";
+  }
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenLogo.src = "../../imagenes/Logo modo oscuro.png";
+  } else {
+    imagenLogo.src = "../../imagenes/logo-azul.png";
+  }
+
+});
+
+
+let BotonVolver = document.getElementById("Crear");
 
 
 function VolverPantalla() {
@@ -8,13 +54,6 @@ function VolverPantalla() {
 }
 
 BotonVolver.addEventListener("click", VolverPantalla);
-
-
-
-
-
-
-
 
 
 let BotonLogo = document.getElementById("Logo");
@@ -25,3 +64,4 @@ function Volver() {
 }
 
 BotonLogo.addEventListener("click", Volver);
+
