657f13ec06d7342e9f4e1df2969a451b63d79958
Your Name <you@example.com>
2025-11-06 10:33:38 -0300

    SoqueTIC funcionaaaaaaaaaaaaaaaa

--- Back-End/Mock/main.js
+++ Back-End/Mock/main.js
@@ -1,9 +1,8 @@
+connect2Server();
 console.log("Frontend cargado correctamente");
 
-const form = document.getElementById("form");
 const nombre = document.getElementById("nombre");
 const email = document.getElementById("email");
-const respuesta = document.getElementById("respuesta");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
@@ -16,8 +15,9 @@ const input = {
   console.log("Enviando al backend:", input)
 
     // Enviamos el evento POST al backend
-    postEvent("registrarUsuario", (input) => {
+    postEvent("registrarUsuario", input, (respuestaBack) => {
         console.log("Respuesta del backend:", input);
         respuesta.innerText = "Backend respondió: " + JSON.stringify(input);
       });
     });
+
