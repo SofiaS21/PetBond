283387ea55c9628924854dc304d8a2b0eb65b66f
SofiaS21 <49876490@est.ort.edu.ar>
2025-11-10 12:04:32 -0300

    pantalla 1 modo oscuro

--- Front-End/Pantalla 1/Inicio.js
+++ Front-End/Pantalla 1/Inicio.js
@@ -48,10 +48,43 @@ function PaginaFavoritos() {
 BotonFavoritos.addEventListener("click", PaginaFavoritos);
 
 const boton = document.querySelector('.boton-modo');
+const imagenFondo = document.getElementById('Fondo');
+const imagenLuna = document.getElementById('Luna');
+const imagenSol = document.getElementById('Sol');
+const imagenPasto = document.getElementById('Pasto');
+
+
 
 boton.addEventListener('click', () => {
   boton.classList.toggle('activo');
   document.body.classList.toggle('dark-mode');
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenFondo.src = "../imagenes/fondo modo oscuro.png";
+  } else {
+    imagenFondo.src = "../imagenes/fondo.png";
+  }
+
+  
+  if (document.body.classList.contains('dark-mode')) {
+    imagenSol.src = "../imagenes/sol modo oscuro.png";
+  } else {
+    imagenSol.src = "../imagenes/sol.png";
+  }
+
+  
+  if (document.body.classList.contains('dark-mode')) {
+    imagenLuna.src = "../imagenes/luna modo oscuro.png";
+  } else {
+    imagenLuna.src = "../imagenes/luna.png";
+  }
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenPasto.src = "../imagenes/Pasto modo oscuro.png";
+  } else {
+    imagenPasto.src = "../imagenes/Pasto.png";
+  }
+
 });
 
 
