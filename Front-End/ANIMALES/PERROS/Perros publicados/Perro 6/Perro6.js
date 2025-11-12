e21f30da428918061241fdeac1034b2f940f0533
SofiaS21 <49876490@est.ort.edu.ar>
2025-11-12 10:41:38 -0300

    mas modo oscuro

--- Front-End/ANIMALES/PERROS/Perros publicados/Perro 6/Perro6.js
+++ Front-End/ANIMALES/PERROS/Perros publicados/Perro 6/Perro6.js
@@ -1,9 +1,48 @@
 const boton = document.querySelector('.boton-modo');
+const imagenLuna = document.getElementById('Luna');
+const imagenSol = document.getElementById('Sol');
+const imagenPasto = document.getElementById('Pasto');
+const imagenLogo = document.getElementById('Logo');
+const imagenCorazon = document.getElementById('corazon');
+
 
 boton.addEventListener('click', () => {
   boton.classList.toggle('activo');
   document.body.classList.toggle('dark-mode');
+  
+  if (document.body.classList.contains('dark-mode')) {
+    imagenSol.src = "../../../../imagenes/sol modo oscuro.png";
+  } else {
+    imagenSol.src = "../../../../imagenes/sol.png";
+  }
+
+  
+  if (document.body.classList.contains('dark-mode')) {
+    imagenLuna.src = "../../../../imagenes/luna modo oscuro.png";
+  } else {
+    imagenLuna.src = "../../../../imagenes/luna.png";
+  }
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenPasto.src = "../../../../imagenes/Pasto modo oscuro.png";
+  } else {
+    imagenPasto.src = "../../../../imagenes/Pasto.png";
+  }
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenLogo.src = "../../../../imagenes/logo modo oscuro.png";
+  } else {
+    imagenLogo.src = "../../../../imagenes/logo-azul.png";
+  }
+
+  if (document.body.classList.contains('dark-mode')) {
+    imagenCorazon.src = "../../../../imagenes/Corazon individual MO.png";
+  } else {
+    imagenCorazon.src = "../../../../imagenes/Corazon.png";
+  }
+
 });
+
 
 let BotonLogo = document.getElementById("Logo");
 
