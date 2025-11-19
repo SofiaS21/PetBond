9479745a7a2cafc910a7a8000182b5c31e5c4928
Your Name <you@example.com>
2025-11-19 11:04:57 -0300

    SokeTIC se rompio :(

---
+++ Back-End/Mock/MockFront.js
@@ -0,0 +1,24 @@
+connect2Server();
+console.log("Frontend cargado correctamente");
+ //sign up
+const Signup = document.getElementById("Signup");
+const SignupNombre = document.getElementById("SignupNombre");
+const SignupEmail = document.getElementById("SignupEmail  ");
+const SignupBoton = document.getElementById("SignupBoton");
+
+
+Signup.addEventListener("SignupBoton", (e) => {
+  e.preventDefault()
+
+const input = {
+    nombre: SignupNombre.value,
+    email: SignupEmail.value
+  };
+
+  console.log("Enviando al backend:", input)
+  
+  if (input.value) {
+    postEvent
+  }
+    });
+
